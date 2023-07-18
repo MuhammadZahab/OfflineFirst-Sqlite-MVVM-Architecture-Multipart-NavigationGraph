65a7697de28e6e3c826f0164b289a353dc6a0086
Muhammad Zahab Ahmad <mzahmad@b2broker.com>
2023-07-18 12:40:50 +0500

    Added comment

--- app/src/main/java/com/example/assessment/application/ApplicationClass.kt
+++ app/src/main/java/com/example/assessment/application/ApplicationClass.kt
@@ -2,7 +2,7 @@ package com.example.assessment.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+//application class
 @HiltAndroidApp
 class ApplicationClass:Application() {
 }
